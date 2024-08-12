a7804b9871f1055476ada4bef5d9435c31d08c52
nitinpai2000 <nitinpai2000@gmail.com>
2024-08-12 19:47:43 +0530

    Custom node added

--- src/Flow.jsx
+++ src/Flow.jsx
@@ -1,57 +1,59 @@
-import { ReactFlow, Controls, Background,applyEdgeChanges, applyNodeChanges,addEdge } from '@xyflow/react';
-import { useCallback,  useState, useEffect } from 'react';
+import { useCallback,useState, useMemo } from 'react';
+import { Handle, Position,ReactFlowProvider, ReactFlow, addEdge, applyEdgeChanges, applyNodeChanges, } from '@xyflow/react';
+import TextUpdaterNode from './TextUpdaterNode.jsx';
 import '@xyflow/react/dist/style.css';
+import './TextUpdater.css';
 
-const initialNodes = [];
+const rfStyle = {
+  backgroundColor: '#B8CEFF',
+};
 
-const initialEdges = [];
+const initialNodes = [
+  {
+    id: 'node-1',
+    type: 'textUpdater',
+    position: { x: 0, y: 0 },
+    data: { value: 123 },
+  },
+];
+ 
 
-export default function Flow() {  
+
+
+export default function Flow({data}) {  
+
+
+  const nodeTypes = useMemo(() => ({ textUpdater: TextUpdaterNode }), []);
 
   const [nodes, setNodes] = useState(initialNodes);
-  const [edges, setEdges] = useState(initialEdges);
-
-  useEffect(() => {
-    console.log("Use effect called");
-    fetch('http://localhost:5146/api/Graph')
-      .then(function(response) {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        var data = response.json();
-        console.log(data);
-        return data;
-      })
-      .then(function(json){
-        console.log(json);
-        setNodes(json.nodes);
-        
-      })
-      .catch(error => console.error(error));      
-  }, []);
+  const [edges, setEdges] = useState([]);
 
   const onNodesChange = useCallback(
     (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
-    [],
+    [setNodes],
   );
   const onEdgesChange = useCallback(
     (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
-    [],
+    [setEdges],
   );
-
+  
   const onConnect = useCallback(
-    (params) => setEdges((eds) => addEdge(params, eds)),
-    [],
+    (connection) => setEdges((eds) => addEdge(connection, eds)),
+    [setEdges],
   );
- 
 
   return (
     <div style={{ height: '100vh', width:'100vw' }}>
-      <ReactFlow nodes={nodes} edges={edges} onNodesChange={onNodesChange} onEdgesChange={onEdgesChange} onConnect={onConnect} fitView>
-        <Background />
-        <Controls />
-      </ReactFlow>
-    </div>
+    <ReactFlow
+    nodes={nodes}
+    edges={edges}
+    onNodesChange={onNodesChange}
+    onEdgesChange={onEdgesChange}
+    onConnect={onConnect}
+    nodeTypes={nodeTypes}
+    fitView
+    style={rfStyle}
+  /></div>
   );
 }
 
