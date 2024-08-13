c4d35cf879e7043fd8189c011ee9e21af149b525
nitinpai2000 <nitinpai2000@gmail.com>
2024-08-13 21:24:59 +0530

    TextUpdater added

--- src/Flow.jsx
+++ src/Flow.jsx
@@ -15,10 +15,26 @@ const initialNodes = [
     position: { x: 0, y: 0 },
     data: { value: 123 },
   },
+  {
+    id: 'node-2',
+    type: 'output',
+    targetPosition: 'top',
+    position: { x: 0, y: 200 },
+    data: { label: 'node 2' },
+  },
+  {
+    id: 'node-3',
+    type: 'output',
+    targetPosition: 'top',
+    position: { x: 200, y: 200 },
+    data: { label: 'node 3' },
+  },
 ];
- 
 
-
+const initialEdges = [
+  { id: 'edge-1', source: 'node-1', target: 'node-2', sourceHandle: 'a' },
+  { id: 'edge-2', source: 'node-1', target: 'node-3', sourceHandle: 'b' },
+];
 
 export default function Flow({data}) {  
 
